4fb09118dc5f4b753438ae667b24d2370ee6c8ec
gsavchenko <george.savchenko@gmail.com>
2016-02-24 20:28:25 -0500

    cleaned code and fixed bug

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -11,7 +11,7 @@ Program	description:    Using the Three.js JavaScript Library and TypeScript, cr
                         solar system should have a central Sun object with at least 5 planets that orbit around it. One of the planets must have a 
                         moon that orbits around it. Include controls that allows the user to zoom the camera out to see the solar system and zoom in 
                         to view the planet with a moon.
-Revision history:       Added zoom functionality, made universe darker
+Revision history:       custodial work done
 
 THREEJS Aliases
 */ 
@@ -48,24 +48,9 @@ var scene: Scene;
 var renderer: Renderer;
 var camera: PerspectiveCamera;
 var axes: AxisHelper;
-var cube: Mesh;
-var head;
-var cubeMaterial;
-var cubeGeometry;
 var sphereMaterial;
 var sphereGeometry;
-var sun;
-var earth;
-var leftLeg;
-var rightLeg;
-var lowerTorso;
-var body;
-var leftArm;
-var rightArm;
-var leftFoot;
-var rightFoot;
 var lamberMaterial;
-var plane: Mesh;
 var sphere: Mesh;
 var ambientLight: AmbientLight;
 var spotLight: SpotLight;
@@ -73,18 +58,17 @@ var pointLight: PointLight;
 var control: Control;
 var gui: GUI;
 var stats: Stats;
-var step: number = 0;
-//var spriteMaterial = THREE.SpriteMaterial;
-var pivot1 = new THREE.Object3D();
-var pivot2 = new THREE.Object3D();
-var pivot3 = new THREE.Object3D();
-var pivot4 = new THREE.Object3D();
-var pivot5 = new THREE.Object3D();
-var pivot6 = new THREE.Object3D();
-var pivot7 = new THREE.Object3D();
-var pivot8 = new THREE.Object3D();
-var pivot9 = new THREE.Object3D();
-var pivot10 = new THREE.Object3D();
+
+var mercuryPivot = new THREE.Object3D();
+var venusPivot = new THREE.Object3D();
+var earthPivot = new THREE.Object3D();
+var marsPivot = new THREE.Object3D();
+var jupiterPivot = new THREE.Object3D();
+var saturnPivot = new THREE.Object3D();
+var uranusPivot = new THREE.Object3D();
+var neptunePivot = new THREE.Object3D();
+var notaPlanetPivot = new THREE.Object3D();
+var moonPivot = new THREE.Object3D();
 
 // Initialize Scene Objects
 function init() {
@@ -98,16 +82,6 @@ function init() {
     axes = new AxisHelper(20);
     scene.add(axes);
     console.log("Added Axis Helper to scene...");
-    
-    // Add a Plane to the Scene (does not rotate with cube man)
-    //  plane = new gameObject(
-    //     new PlaneGeometry(60, 40, 1, 1),
-    //    new LambertMaterial({ color: 0x66cd00 }),
-    //    0, 0, 0);
-
-   // plane.rotation.x = -0.5 * Math.PI;
-   // scene.add(plane);
-   // console.log("Added Plane Primitive to scene...");
      
     //Add point light
     pointLight = new PointLight(0xffffff, 1, 0);
@@ -127,16 +101,15 @@ function init() {
     scene.add(spotLight);
     console.log("Added a SpotLight Light to Scene");
     
-        // Add a SpotLight to the scene
+    // Add a SpotLight to the scene
     spotLight = new SpotLight(0xffffff);
     spotLight.position.set(-85, -60, 85);
    // spotLight.castShadow = true;
     scene.add(spotLight);
     console.log("Added a SpotLight Light to Scene");
    
-   addPlanets();
-    
-        
+    addPlanets();
+           
     // Add controls using DAT.GUI to allow user to rotate cube man
     gui = new GUI();
     control = new Control();
@@ -153,137 +126,124 @@ function init() {
 }
 
 function addPlanets(): void{
-    
-    //Sun Texture
-   // sun = new THREE.Mesh(
-    //    new SphereGeometry(10, 20, 20),
-    //    new THREE.MeshPhongMaterial({
-    //        map: THREE.ImageUtils.loadTexture('Content/Textures/sun.png'),
-     //       shininess: 100,
-     //   })
-   // );
-   // scene.add(sun);
-    
-    //Sun
+   
+    // Sun
     sphereGeometry = new SphereGeometry(10, 20, 20);
     sphereMaterial = new LambertMaterial({color: 0xfd8813});    
-    sun = new Mesh(sphereGeometry, sphereMaterial);
+    var sun = new Mesh(sphereGeometry, sphereMaterial);
     scene.add(sun);
     
-    //Earth
-    sphereGeometry = new SphereGeometry(0.92, 20, 20);
-    sphereMaterial = new LambertMaterial({color: 0x0000a0});   
-    earth = new Mesh(sphereGeometry, sphereMaterial);
-    scene.add(earth);
-    scene.updateMatrixWorld(true);
-
-    pivot1.rotation.z = 0;
-    pivot2.rotation.z = 2 * Math.PI / 3;
-    pivot3.rotation.z = 4 * Math.PI / 3;
-    pivot4.rotation.z = 6 * Math.PI / 3;
-    pivot5.rotation.z = 8 * Math.PI / 3;
-    pivot6.rotation.z = 10 * Math.PI / 3;
-    pivot7.rotation.z = 12 * Math.PI / 3;
-    pivot8.rotation.z = 14 * Math.PI / 3;
-    pivot9.rotation.z = 16 * Math.PI / 3;
-    pivot10.rotation.z = 0;
-    
-    sun.add(pivot1);
-    sun.add(pivot2);
-    sun.add(pivot3);
-    sun.add(pivot4);
-    sun.add(pivot5);
-    sun.add(pivot6);
-    sun.add(pivot7);
-    sun.add(pivot8);
-    sun.add(pivot9);    
-    earth.add(pivot10);
-    
-    //mesh
-    
+    // Mercury 
     sphereGeometry = new SphereGeometry(0.36, 20, 20);
     sphereMaterial = new LambertMaterial({color: 0xe6e6e6});    
     var mercury = new Mesh(sphereGeometry, sphereMaterial);
     
+    sun.add(mercuryPivot);
+    mercuryPivot.rotation.z = 0;
+    mercury.position.y = 0;
+    mercury.position.z = 11.7;
+    mercuryPivot.add(mercury);
+    
+    // Venus
     sphereGeometry = new SphereGeometry(0.88, 20, 20);
     sphereMaterial = new LambertMaterial({color: 0xa57c1b});    
     var venus = new Mesh(sphereGeometry, sphereMaterial);
     
-   // sphereGeometry = new SphereGeometry(0.92, 20, 20);
-   // sphereMaterial = new LambertMaterial({color: 0x0000a0});   
-   // var earth = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    sun.add(venusPivot);
+    venusPivot.rotation.z = 2 * Math.PI / 3;    
+    venus.position.y = 0;
+    venus.position.z = 13.2;
+    venusPivot.add(venus);
     
+    // Earth
+    sphereGeometry = new SphereGeometry(0.92, 20, 20);
+    sphereMaterial = new LambertMaterial({color: 0x0000a0});   
+    var earth = new Mesh(sphereGeometry, sphereMaterial);
+    scene.add(earth);
+    
+    sun.add(earthPivot);
+    earthPivot.rotation.z = 4 * Math.PI / 3;
+    earth.position.y = 0;
+    earth.position.z = 15.4;
+    earthPivot.add(earth);
+    
+    // Moon
     sphereGeometry = new SphereGeometry(0.2, 20, 20);
     sphereMaterial = new LambertMaterial({color: 0xfefcd7});   
     var moon = new THREE.Mesh(sphereGeometry, sphereMaterial);
     
+    earth.add(moonPivot);
+    moonPivot.rotation.z = 0;
+    moon.position.y = 0;
+    moon.position.z = 3;
+    moonPivot.add(moon);
+    
+    // Mars
     sphereGeometry = new SphereGeometry(0.48, 20, 20);
     sphereMaterial = new LambertMaterial({color: 0xa1251b});   
     var mars = new THREE.Mesh(sphereGeometry, sphereMaterial);
     
+    sun.add(marsPivot);
+    marsPivot.rotation.z = 6 * Math.PI / 3;
+    mars.position.y = 0;
+    mars.position.z = 17.8;
+    marsPivot.add(mars);
+    
+    // Jupiter
     sphereGeometry = new SphereGeometry(1.03, 20, 20);
     sphereMaterial = new LambertMaterial({color: 0xd8ca9d});   
     var jupiter = new THREE.Mesh(sphereGeometry, sphereMaterial);
     
+    sun.add(jupiterPivot);
+    jupiterPivot.rotation.z = 8 * Math.PI / 3;      
+    jupiter.position.y = 0;
+    jupiter.position.z = 20.7;
+    jupiterPivot.add(jupiter);
+    
+    // Saturn
     sphereGeometry = new SphereGeometry(0.87, 20, 20);
     sphereMaterial = new LambertMaterial({color: 0xa1251b});   
     var saturn = new THREE.Mesh(sphereGeometry, sphereMaterial);
     
+    sun.add(saturnPivot);
+    saturnPivot.rotation.z = 10 * Math.PI / 3;
+    saturn.position.y = 0;
+    saturn.position.z = 33.8;
+    saturnPivot.add(saturn);
+    
+    // Uranus
     sphereGeometry = new SphereGeometry(0.37, 20, 20);
     sphereMaterial = new LambertMaterial({color: 0xa1251b});   
     var uranus = new THREE.Mesh(sphereGeometry, sphereMaterial);
     
+    sun.add(uranusPivot);
+    uranusPivot.rotation.z = 12 * Math.PI / 3;
+    uranus.position.y = 0;
+    uranus.position.z = 63;
+    uranusPivot.add(uranus);
+    
+    // Neptune
     sphereGeometry = new SphereGeometry(0.36, 20, 20);
     sphereMaterial = new LambertMaterial({color: 0xa1251b});   
     var neptune = new THREE.Mesh(sphereGeometry, sphereMaterial);
     
+    sun.add(neptunePivot);
+    neptunePivot.rotation.z = 14 * Math.PI / 3;
+    neptune.position.y = 0;
+    neptune.position.z = 95.7;
+    neptunePivot.add(neptune);
+    
+    // Pluto
     sphereGeometry = new SphereGeometry(0.05, 20, 20);
     sphereMaterial = new LambertMaterial({color: 0xa1251b});   
     var notaPlanet = new THREE.Mesh(sphereGeometry, sphereMaterial);
     
-    mercury.position.y = 0;
-    venus.position.y = 0;
-    earth.position.y = 0;
-    moon.position.y = 0;
-    mars.position.y = 0;
-    jupiter.position.y = 0;
-    saturn.position.y = 0;
-    uranus.position.y = 0;
-    neptune.position.y = 0;
+    sun.add(notaPlanetPivot);
+    notaPlanetPivot.rotation.z = 16 * Math.PI / 3;   
     notaPlanet.position.y = 0;
-    
-        
-    mercury.position.z = 11.7;
-    venus.position.z = 13.2;
-    earth.position.z = 15.4;
-    moon.position.z = 3;
-    mars.position.z = 17.8;
-    jupiter.position.z = 20.7;
-    saturn.position.z = 33.8;
-    uranus.position.z = 63;
-    neptune.position.z = 95.7;
     notaPlanet.position.z = 125;
-    
-    pivot1.add(mercury);
-    pivot2.add(venus);
-    pivot3.add(earth);
-    pivot4.add(mars);
-    pivot5.add(jupiter);
-    pivot6.add(saturn);
-    pivot7.add(uranus);
-    pivot8.add(neptune);
-    pivot9.add(notaPlanet);
-    pivot10.add(moon);
-    
-    
-   // var spriteMaterial = new THREE.SpriteMaterial({
-   //     map: THREE.ImageUtils.loadTexture('textures/glow.png'),
-   //     color: 0x0000ff, transparent: false, blending: THREE.AdditiveBlending
-  //  });
-   // var sprite = new THREE.Sprite(spriteMaterial);
-   // sprite.scale.set(200, 200, 1.0);
-   // sun.add(sprite);
-    
+    notaPlanetPivot.add(notaPlanet);  
+
 }
 
 // Change camera view based on browser window size
@@ -312,26 +272,21 @@ function addStatsObject() {
 function gameLoop(): void {
     stats.update();
     
-    pivot1.rotation.y += 0.025;
-    pivot2.rotation.y += 0.022;
-    pivot3.rotation.y += 0.02;
-    pivot4.rotation.y += 0.019;
-    pivot5.rotation.y += 0.016;
-    pivot6.rotation.y += 0.013;
-    pivot7.rotation.y += 0.01;
-    pivot8.rotation.y += 0.015;
-    pivot9.rotation.y += 0.001;
-    pivot10.rotation.y += 0.03;
+    mercuryPivot.rotation.y += 0.025;
+    venusPivot.rotation.y += 0.022;
+    earthPivot.rotation.y += 0.02;
+    marsPivot.rotation.y += 0.019;
+    jupiterPivot.rotation.y += 0.016;
+    saturnPivot.rotation.y += 0.013;
+    uranusPivot.rotation.y += 0.01;
+    neptunePivot.rotation.y += 0.015;
+    notaPlanetPivot.rotation.y += 0.001;
+    moonPivot.rotation.y += 0.03;
     
     camera.position.x = control.positionX;
     camera.position.y = control.positionY;
     camera.position.z = control.positionZ;
     camera.lookAt(control.face);
-    
-   //set body mesh to rotate based on control panel changes
-  //  body.rotation.x += control.rotationSpeedx;
-   // body.rotation.y += control.rotationSpeedy;
-   // body.rotation.z += control.rotationSpeedz;
     
     // render using requestAnimationFrame
     requestAnimationFrame(gameLoop);
@@ -352,10 +307,6 @@ function setupRenderer(): void {
 // Setup main (perspective) camera for the scene
 function setupCamera(): void {
     camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-    
-   // camera.position.x = -125;
-    //camera.position.y = 135;
-   //camera.position.z = 125;
     
     camera.lookAt(new Vector3(5, 0, 0));
     console.log("Finished setting up Camera...");
--- Scripts/objects/control.ts
+++ Scripts/objects/control.ts
@@ -9,7 +9,7 @@ Program	description:    Using the Three.js JavaScript Library and TypeScript, cr
                         solar system should have a central Sun object with at least 5 planets that orbit around it. One of the planets must have a 
                         moon that orbits around it. Include controls that allows the user to zoom the camera out to see the solar system and zoom in 
                         to view the planet with a moon.
-Revision history:       Added zooming functionality
+Revision history:       fixed bug and made look not like vomit
 */
 
 module objects {
@@ -34,25 +34,21 @@ module objects {
        
        // This method uses a random number from 1 - 3 to set the shirtColour and pantsColour to a predetermind set of colors
        public zoomInOut() : void {       
-        if(!this.zoom)
-        {
-                                this.positionX = -40;
-                    this.positionY = 5;
-                    this.positionZ = -20;
-                    this.face = new Vector3(0, -1, 45);
-                    this.zoom = false;
-                    console.log(this.zoom);
-
+        if(!this.zoom)        {
+            this.positionX = -40;
+            this.positionY = 5;
+            this.positionZ = -20;
+            this.face = new Vector3(0, -1, 45);
+            this.zoom = true;
+            console.log(this.zoom);
         }
-        else
-        {
-                                    this.positionX = -85;
-                    this.positionY = 95;
-                    this.positionZ = 85;
-                    this.face = new Vector3(5, 0, 0);
-                    this.zoom = true;
-                    console.log(this.zoom);
-        }
-        };
+        else{
+            this.positionX = -85;
+            this.positionY = 95;
+            this.positionZ = 85;
+            this.face = new Vector3(5, 0, 0);
+            this.zoom = false;
+            console.log(this.zoom);
+        }};
     }
 }
